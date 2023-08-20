24d50700abd07e4c8e8000b38bfff5fe03229b9c
JoeCool-Engineer <rodney.buller2+github@gmail.com>
2023-08-20 09:21:17 -0700

    Initial Push of Code

---
+++ index.js
@@ -0,0 +1,30 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = [['input','name', 'What is the title of this project?'],
+['input','description', 'Enter a brief desription of this project.'],
+['input', 'usage', 'Provide instructions and examples for use.'],
+['input', 'contribution', 'Provide guidance on how to contribute to this project.'],
+['input', 'test', 'How to run test for this project.'],
+['list', 'license', 'Which license?', ['None', 'Mit', 'ISC', 'Mozilla']],
+['list', 'color', 'Which color for the badge?', ['None', 'blue', 'red', 'yellow', 'green']]
+];  
+
+inquirer.prompt(questions.map((question) => ({
+    type: question[0],
+    name: question[1],
+    message: question[2],
+    choices: question[3],
+})))
+.then((userResponse) => { 
+    console.log(userResponse) 
+    const readMeContent = generateMarkdown(userResponse);
+
+    fs.writeFile('README.md', readMeContent, (err) =>
+    err ? console.log(err) : console.log('Successfully created index.html!')
+  );
+})
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,70 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// Color Picker
+function colorOptions(color) {
+  if ( color !== "None") {
+    return `${ color }.svg)`
+  } else {
+    return "inactive.svg)"
+  }
+}
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if ( license !== "None") {
+    return `![License](https://img.shields.io/badge/License-${ license }-`
+  } else {
+    return ""
+  }
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if ( license !== "None") {
+    return `- [License](#license)`
+  } else {
+    return ""
+  }
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if ( license !== "None") {
+    return `## License`
+  } else {
+    return ""
+  }
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown({ name, description, usage, contribution, test, license, color }) {
+  return `# ${ name }
+  ${renderLicenseBadge(license)}${colorOptions(color)}
+
+  ## Description
+  ${ description }
+  
+  ## Table of Contents 
+
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Test](#test)
+  ${renderLicenseLink(license)}
+  
+  ## Usage
+  ${ usage }
+  
+  ## Contribution
+  ${ contribution }
+  
+  ## Test
+  ${ test }
+  
+  ${ renderLicenseSection(license) }
+
+  The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).`
+  
+  // `# ${data.title}`;
+}
+
+module.exports = generateMarkdown;
